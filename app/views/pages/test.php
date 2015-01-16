b4eae199fdb2d579478f3185404475d504bf42ad
jimmysscentaur <jimmysscentaur@gmail.com>
2015-01-16 16:44:54 +0700

    fix-display-calendar

--- app/views/pages/test.php
+++ app/views/pages/test.php
@@ -1,5 +1,103 @@
-@extends('layouts.default')
-@section('content')	
+
+<!doctype html>
+<html>
+<head>
+    <!-- Material Design for Bootstrap -->
+	<!-- Mobile support -->
+  	<title>Booking</title>
+    <!-- Twitter Bootstrap -->
+   	<link href="http://localhost/Project2/booking/public/home_page/css/material/bootstrap.min.css" rel="stylesheet"> 
+
+    <!-- Material Design for Bootstrap -->
+    <link href="http://localhost/Project2/booking/public/home_page/css/material/material-wfont.min.css" rel="stylesheet">
+    <link href="http://localhost/Project2/booking/public/home_page/css/material/ripples.min.css" rel="stylesheet">
+ 	<!-- jQuery -->
+    <script src="http://localhost/Project2/booking/public/home_page/js/material/jquery-1.10.2.min.js"></script>
+    <!-- Dropdown.js -->
+    <link href="http://localhost/Project2/booking/public/home_page/css/material/jquery.dropdown.css" rel="stylesheet">
+
+    <!-- Twitter Bootstrap -->
+    <script src="http://localhost/Project2/booking/public/home_page/js/material/bootstrap.min.js"></script>
+
+    <!-- Material Design for Bootstrap -->
+    <script src="http://localhost/Project2/booking/public/home_page/js/material/material.min.js"></script>
+    <script src="http://localhost/Project2/booking/public/home_page/js/material/ripples.min.js"></script>
+    <!-- Sliders -->
+    <!-- Dropdown.js -->
+    <script src="http://localhost/Project2/booking/public/home_page/js/material/jquery.dropdown.js"></script>
+    <script src="http://localhost/Project2/booking/public/home_page/js/include-header.js"></script>
+	<!--  end Material Design for Bootstrap -->
+	<link href="http://localhost/Project2/booking/public/home_page/css/font_manifest-15c87676a32319c4cbdb2a9c1e3f9eb2.css" media="screen" rel="stylesheet" type="text/css" />
+
+	<link href="http://localhost/Project2/booking/public/home_page/css/application.css?12" media="all" rel="stylesheet" type="text/css" /> 
+	<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" media="screen" rel="stylesheet" type="text/css" />
+	<link href="http://localhost/Project2/booking/public/home_page/css/ng-dialog.min.css" media="screen" rel="stylesheet" type="text/css" />
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        
+    <link rel="stylesheet" href="http://localhost/Project2/booking/public/home_page/css/normalize.css?12">   
+    <link rel="stylesheet" href="http://localhost/Project2/booking/public/home_page/css/main.css?1">
+    <script src="http://localhost/Project2/booking/public/home_page/js/ga.js"></script>
+    <script src="http://localhost/Project2/booking/public/home_page/js/modernizr-2.7.1.min.js"></script>
+    <link href="http://localhost/Project2/booking/public/home_page/css/include-head.css?12" rel="stylesheet"> 
+
+	<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+  
+  	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+  
+
+
+  
+ 
+ <!-- calendar -->
+ <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+ <!-- end Calendar -->
+</head>
+<body>  
+	<header class="page-header" style= "display: inline; ">
+  	<div class="row" id ="rowNarbar" >
+		<!-- navbar navbar-default -->
+		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<!-- navbar-header -->
+			<div class="navbar-header" style ="background-color: #2ecc71;">				
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href=http://localhost/Project2/booking/public>
+	        		LOGO
+	      		</a>
+			</div><!-- /navbar-header -->
+			<!-- navbar -->
+			<div class="navbar-collapse collapse" style ="background-color: #2ecc71;">
+				<ul class="nav navbar-nav navbar-right" style ="padding-top: 0px; padding-right: 20px;">
+				 	<li><a href=http://localhost/Project2/booking/public/about> About Us</a></li>
+				 	<li><a href=http://localhost/Project2/booking/public/why-book-with-us> Why book with us?</a></li>
+					<li><a href= http://localhost/Project2/booking/public/login > Sign-in </a></li>
+					
+	                 <li><a href=http://localhost/Project2/booking/public/FAQ> FAQ</a></li>
+	                
+		            
+	                <li class="dropdown">
+	                 	<a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Language <b class="caret"></b><div class="ripple-wrapper"></div></a>
+                      	<ul class="dropdown-menu">
+                        <li><a href=http://localhost/Project2/booking/public/language/english>English</a></li>
+                        <li><a href=http://localhost/Project2/booking/public/language/germany>Vietnamese </a></li>
+                      	</ul>
+	                </li>
+			</div><!-- /navbar -->				
+		</div><!-- /navbar navbar-default -->
+	</div>
+</header>
+    	
 	<main>   	 
     <section id="slide-1">
 		<div class="bcg skrollable skrollable-between" data-center="background-position: 50% 0px;" data-top-bottom="background-position: 50% -100px;" data-anchor-target="#slide-1" id = "bcg-slide1-plusCss">
@@ -23,7 +121,7 @@
 								</div>
 								<div class=".col-lg-6 col-md-6 col-sm-6 col-xs-12 thumb">
 									<form class="search">
-										<input class="searchTerm" placeholder="Please type ...">
+										<input class="searchTerm" placeholder="Typing ...">
 										<input class="searchButton" type="">
 										<span class="searchIcon"></span>
 									</form>
@@ -90,39 +188,16 @@
 											</div>
  
 								
-  											
-  											<!-- <p>Date: <input type="text" id="datepicker"></p> -->
+  											<p>Date: <input type="text" id="datepicker"></p>
   											   <script>
 											    $(document).ready(function() {
 												    $("#datepicker").datepicker();
 												})
 											  </script>	
-											  <input id="datepicker" type = "text" placeholder = "mm/dd/yyyy" style = "-webkit-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
-													-webkit-padding-end: 20px;
-													-webkit-padding-start: 2px;
-													background-position: 97% center;
-													background-repeat: no-repeat;
-													border: 1px solid #AAA;
-													font-size: inherit;
-													overflow: hidden;
-													padding: 5px 5px;
-													text-overflow: ellipsis;
-													white-space: nowrap;
-													width: 80%;
-													line-height: 25px;
-													color: #fff;
-													background-image: url(http://i62.tinypic.com/15xvbd5.png),-webkit-linear-gradient(#2ECC71, #289E5A 40%, #2ECC71);
-													background-color: #2ECC71;
-													border-radius: 20px;
-													padding-left: 15px;
-													">
-													
-
+											<input class = "soflow-color " id="soflow-color-date" type = "date">
 											
 										</div>
 									</div>
-								
-									
 								
 									<div class = "row">
 										<div class=".col-lg-6 col-md-6 col-sm-6 col-xs-12 thumb">
@@ -277,20 +352,20 @@
 								
 								<hr class="section-heading-spacer">
 								<div class="clearfix"></div>
-								<p class='home-download-app-header color-53A524' >{{trans ('greetings.Claritas est etiam')}}</p>
-								<p class = "bigtext color-53A524">{{trans ('greetings.Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.')}} 
-									<a href="https://miratik.com/">{{trans ('greetings.Mirum est notare quam littera gothica')}}</a> {{trans ('greetings.est etiam processus.')}}
+								<p class='home-download-app-header color-53A524' >Claritas est etiam</p>
+								<p class = "bigtext color-53A524">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. 
+									<a href="https://miratik.com/">Mirum est notare quam littera gothica</a> est etiam processus.
 								</p>
 							</div>
 						</div>
 					</div>
 					<div class='main-content-row'>
 						<div class='row'>
-							<h3 class='section-block-header pad-top color-white style-Shojumaru'>{{trans ('greetings.Ready to start?')}}</h3>
+							<h3 class='section-block-header pad-top color-white style-Shojumaru'>Ready to start?</h3>
 						</div>
 						<div class='small-12 columns two-button-row' id = "main-content-slides3-plusCss">
 							<a href="#" class="big-button btn-secondary call-to-action btn_SeeAll_fisrt style-Shojumaru">Getting Started</a>
-							<a href="{{URL::to('list-service')}}" class="big-button btn-invert-secondary call-to-action btn_SeeAll_second style-Shojumaru">All Us Services</a>
+							<a href="http://localhost/Project2/booking/public/list-service" class="big-button btn-invert-secondary call-to-action btn_SeeAll_second style-Shojumaru">All Us Services</a>
 						</div>
 					</div>
 				</div>
@@ -298,14 +373,60 @@
 		</div>
 	</section> 
 	</main>
-	
-	
-	<script src="{{ asset('home_page/js/include-landing.js?240') }}"></script>
-	<!-- <script src="{{ asset('home_page/js/jquery.min.js') }}"></script> -->
+	<script src="http://localhost/Project2/booking/public/home_page/js/include-landing.js?133"></script>
+	<!-- 1<script src="http://localhost/Project2/booking/public/home_page/js/jquery.min.js"></script>-->
 	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-	<script src="{{ asset('home_page/js/imagesloaded.js') }}"></script>
-	<script src="{{ asset('home_page/js/skrollr.js') }}"></script>
-	<script src="{{ asset('home_page/js/_main.js') }}"></script>
-	<link rel="stylesheet" href="{{ asset('home_page/css/include-landing.css?2') }}">
+	<script src="http://localhost/Project2/booking/public/home_page/js/imagesloaded.js"></script>
+	<script src="http://localhost/Project2/booking/public/home_page/js/skrollr.js"></script>
+	<script src="http://localhost/Project2/booking/public/home_page/js/_main.js"></script>
+	<link rel="stylesheet" href="http://localhost/Project2/booking/public/home_page/css/include-landing.css">
 
-@stop
+    
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row" style="margin-top: 15px;">
+                    <div class="footer-col col-md-4">
+                        <h3>Location</h3>
+                        <p>1234 Address<br>City</p>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Around the Web</h3>
+                        <ul class="list-inline">
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <a href = http://localhost/Project2/booking/public/contact><h3 style = "color: #FFF">Contact Us</h3></a>
+                        <a href = http://localhost/Project2/booking/public/terms><h4 style = "color: #FFF;margin: 5px 0">Terms and Conditions</h4></a>
+                        <a href = http://localhost/Project2/booking/public/privacy><h4 style = "color: #FFF;margin: 5px 0">Privacy Policy</h4></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row"style="margin-top: 10px;">
+                    <div class="col-lg-12">
+                        Copyright &copy; Miratik 2015
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer></body>
+</html>
