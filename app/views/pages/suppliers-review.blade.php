e89e04dc8873012a6793473a746a5e9236e87f50
thinh voxuan <thinhvoxuan@gmail.com>
2015-01-24 01:08:58 +0700

    + layout supplier-review page

--- app/views/pages/suppliers-review.blade.php
+++ app/views/pages/suppliers-review.blade.php
@@ -1,56 +1,66 @@
 @extends('layouts.default')
 @section('content')
 <link href="{{ cached_asset('css/jRating.jquery.css', true) }}" rel="stylesheet"/>
+<link href="{{ cached_asset('/css/supplier.css', true) }}" rel="stylesheet"/>
     <div class="main-body-container" style="padding-top: 80px;padding-bottom: 20px">
-        <div class="row">
-        </div>
-        <div class="row" style="padding: 0 0 100px;background-color: #ffffff;color: rgba(0,0,0,.84); padding-top:20px">
+        <div class="row" style="background-color: #ffffff;color: rgba(0,0,0,.84); padding-top:20px">
+            <div class="review-review col-md-12">
+                <div class="row">
+                    <div class="review-header col-md-12">
+                        <h1>Review for</h1>
+                        <div class="row">
+                            <div class="col-md-3 vcenter">
+                                <a href="#" class="thumbnail">
+                                    <img data-src="holder.js/140%x100" alt="140%x100"
+                                         src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTM0IiBoZWlnaHQ9IjE4MCIgdmlld0JveD0iMCAwIDEzNCAxODAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMTM0IiBoZWlnaHQ9IjE4MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQxLjA0Njg3NSIgeT0iOTAiIHN0eWxlPSJmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MTBwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj4xMzR4MTgwPC90ZXh0PjwvZz48L3N2Zz4="
+                                         data-holder-rendered="true"
+                                         style="height: 100px; width: 140px; display: block;">
+                                </a>
+                            </div>
+                            <div class="col-md-3 vcenter">
+                                <h3>name of supplier</h3>
+                            </div>
+                            <div class="col-md-3 col-md-offset-3 vcenter" style="width: 23%">
+                                <a class="btn btn-default btn-primary" href="{{route('suppliers');}}" role="button">Back to results</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="review-content col-md-12">
+                        @for($i =0; $i< 10; $i++)
+                            <div class="row" style="margin-left: 0; margin-right: 0; margin-bottom: 15px; border: 1px solid; padding-bottom: 15px">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <p>"CUSTOMER NAME" said:</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>Title of review (e.g. Horrible service, rude staff)</p>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div>
+                                                <img alt="1" src="/css/icons/star-on.png" title="poor">&nbsp;
+                                                <img alt="1" src="/css/icons/star-on.png" title="poor">&nbsp;
+                                                <img alt="1" src="/css/icons/star-on.png" title="poor">&nbsp;
+                                                <img alt="1" src="/css/icons/star-on.png" title="poor">&nbsp;
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            Way nor furnished sir procuring therefore but. Warmth far manner myself active are cannot called. Set her half end girl rich met. Me allowance departure an curiosity ye. In no talking address excited it conduct. Husbands debating replying overcame blessing he it me to domestic.
+                                        </div>
+                                    </div>
+                                    <div class="row">
+
+                                    </div>
+                                </div>
+                            </div>
+                        @endfor
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-<script src="{{ cached_asset('/home_page/js/jquery.min.js', true) }}"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-<script src="{{ cached_asset('/home_page/js/imagesloaded.js', true) }}"></script>
-<script src="{{ cached_asset('/home_page/js/skrollr.js', true) }}"></script>
-<script src="{{ cached_asset('/home_page/js/_main.js', true) }}"></script>
-
-<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-<script src="{{ cached_asset('js/jRating.jquery.min.js', true) }}"></script>
-<script src="{{ cached_asset('js/jquery.raty.js', true) }}"></script>
-<script type="text/javascript">
-    $("#datepicker").datepicker();
-    $(document).ready(function(){
-        // simple jRating call
-        $(".basic").jRating({
-            bigStarsPath: '/css/icons/stars.png',
-            smallStarsPath: '/css/icons/small.png',
-            length: 5,
-            isDisabled: true
-        });
-//        $("div.filter-result-star").raty({
-//            number: function() {
-//                return $(this).attr('data-id');
-//            },
-//            score :function(){
-//                return $(this).attr('data-id');
-//            },
-//            readOnly: true
-//        });
-        $( "#slider-range" ).slider({
-            range: true,
-            min: 0,
-            max: 500,
-            values: [ 0, 500 ],
-            slide: function( event, ui ) {
-                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-                $("input#from-price").val(ui.values[ 0 ])
-                $("input#to-price").val(ui.values[ 1 ])
-            }
-        });
-        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-    });
-</script>
 @stop
